cc0c9fb5390375eb1a0f91c347d640574c967e8b
Weronika Chmielewska <weronika.chmieleska00@gmail.com>
2022-04-23 16:17:15 +0200

    js add function

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-let amountElement = document.querySelector(".js-form__amount");
-let currencyElement = document.querySelector(".js-form__inputPLN");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-form__result");
+{
+    let currencyElement = document.querySelector(".js-form__inputPLN");
+    
+    const formElement = document.querySelector(".js-form");
+    const selectElement = document.querySelector(".js-form__currencySelect");
+    const resultElement = document.querySelector(".js-form__result");
 
-let selectElement = document.querySelector(".js-form__currencySelect");
-let plnElement = document.querySelector("js-form__PLN");
-let KRWElement = document.querySelector("js-form__KRW");
-
-
-
-
-formElement.addEventListener("input", () => {
-    switch (selectElement.value) {
-        case "PLN":
-            currencyElement = document.querySelector(".js-form__inputPLN");
-            document.getElementById("KRW").style.display = "none";
-            document.getElementById("PLN").style.display = "block";
-            currencyText = "KRW";
-            break;
-        case "KRW":
-            currencyElement = document.querySelector(".js-form__inputKRW");
-            document.getElementById("PLN").style.display = "none";
-            document.getElementById("KRW").style.display = "block";
-            currencyText = "PLN";
-            break;
+    
+    const changeLabels = () => {
+        formElement.addEventListener("input", () => {
+            switch (selectElement.value) {
+                case "PLN":
+                    currencyElement = document.querySelector(".js-form__inputPLN");
+                    document.getElementById("KRW").style.display = "none";
+                    document.getElementById("PLN").style.display = "block";
+                    currencyText = "KRW";
+                    break;
+                case "KRW":
+                    currencyElement = document.querySelector(".js-form__inputKRW");
+                    document.getElementById("PLN").style.display = "none";
+                    document.getElementById("KRW").style.display = "block";
+                    currencyText = "PLN";
+                    break;
+            }
+        });
     }
-});
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    changeLabels();
 
-    let amount = amountElement.value;
-    let currency = currencyElement.value;
+    const onFormSubmit = () => {
+        formElement.addEventListener("submit", (event) => {
+            event.preventDefault();
+    
+            const amountElement = document.querySelector(".js-form__amount");
+            
+            const amount = +amountElement.value;
+            const currency = currencyElement.value;
+            
+    
+            const result = amount * currency;
+    
+            resultElement.innerText = result.toFixed(4) + " " + currencyText;
+        });
+    }
 
-    let result = amount * currency;
+    onFormSubmit();
 
-    resultElement.innerText = result.toFixed(4) + " " + currencyText;
-});
-
-formElement.addEventListener("reset", () => {
-    resultElement.innerText = "N/A";
-});
+    const resetResult = () => {
+        formElement.addEventListener("reset", () => {
+            resultElement.innerText = "N/A";
+        });
+    }
+    
+    resetResult();
+}
